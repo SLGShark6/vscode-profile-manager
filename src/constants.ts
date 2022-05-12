d8a9b06e6e055d2182b5e8b67b3b217241c1b1f3
Shane Gardner <gardnershane@outlook.com>
2022-05-12 23:51:14 +0000

    Update constants to export individual items and added extension configuration keys

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,22 @@
+import { Dictionary } from "@extension/utilities";
 
 /**
- * Readonly extension configuration object
+ * The internal extension ID
  */
-export const constants = {
-   internalName: "profile-manager",
+export const internalName = "profile-manager";
 
-   displayName: "Profile Manager"
-};
+/**
+ * The human readable extension name
+ */
+export const displayName = "Profile Manager";
+
+/**
+ * Keyed list of all registered configuration contributions for the extension
+ */
+export const configurationKeys
+   : Readonly<Dictionary<"ActiveProfile" | "ProfilesList" | "IgnoreExtensions" | "IgnoreSettings", string>> = {
+   ActiveProfile: `${internalName}.active-profile`,
+   ProfilesList: `${internalName}.profiles`,
+   IgnoreExtensions: `${internalName}.ignore-extensions`,
+   IgnoreSettings: `${internalName}.ignore-settings`
+}
