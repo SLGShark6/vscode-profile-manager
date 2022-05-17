805ccd349c4f4a32b196ecbcbee72145ce129ef7
Shane Gardner <gardnershane@outlook.com>
2022-05-17 00:29:36 +0000

    Change vscode barrel import to individual imports in extension entrypoint

--- src/extension.ts
+++ src/extension.ts
@@ -3,20 +3,20 @@ import "core-js/proposals/reflect-metadata";
 import { container } from "tsyringe";
 
 // The module 'vscode' contains the VS Code extensibility API
-import * as vscode from 'vscode';
+import { ExtensionContext, commands } from 'vscode';
 
 import * as constants from "@extension";
 import { ConfigHelper, ExtensionHelper, ProfileHelper } from '@extension/helpers';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
    container.registerSingleton<ConfigHelper>(ConfigHelper);
    container.registerSingleton<ProfileHelper>(ProfileHelper);
    container.registerSingleton<ExtensionHelper>(ExtensionHelper);
 
-   container.registerInstance<vscode.ExtensionContext>("ExtensionContext", context);
+   container.registerInstance<ExtensionContext>("ExtensionContext", context);
    
 
    // let helper = container.resolve<ProfileHelper>(ProfileHelper);
@@ -24,16 +24,16 @@ export function activate(context: vscode.ExtensionContext) {
 
    // ToDo wrap this conditionally with whether this is startup
    // ToDo set this conditionally from the config helper 
-   vscode.commands.executeCommand('setContext', `${constants.internalName}.profileLoaded`, true);
+   commands.executeCommand('setContext', `${constants.internalName}.profileLoaded`, true);
 
 	context.subscriptions.push(
-      vscode.commands.registerCommand(`${constants.internalName}.saveProfile`, async () => {
+      commands.registerCommand(`${constants.internalName}.saveProfile`, async () => {
          await saveProfileHandler();
 	   })
    );
 
    context.subscriptions.push(
-      vscode.commands.registerCommand(`${constants.internalName}.loadProfile`, async () => {
+      commands.registerCommand(`${constants.internalName}.loadProfile`, async () => {
 		   await loadProfileHandler();
 	   })
    );
