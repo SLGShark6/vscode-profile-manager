18642b31047cb52a4158d615c14446cd35da5748
Shane Gardner <gardnershane@outlook.com>
2022-05-17 00:33:27 +0000

    Add functions for working with extension in ExtensionHelper and defined getInstalledExtensions() and getInstalledExtensionIds()

--- src/helpers/extension.helper.ts
+++ src/helpers/extension.helper.ts
@@ -1,4 +1,6 @@
-import { injectable } from "tsyringe";
+import { isEqual } from "lodash";
+import { inject, injectable } from "tsyringe";
+import { Extension, ExtensionContext, extensions } from "vscode";
 
 @injectable()
 export class ExtensionHelper {
@@ -6,8 +8,30 @@ export class ExtensionHelper {
    /**
     * Ctor
     */
-   constructor() {
+   constructor(
+      @inject("ExtensionContext") private _extensionContext: ExtensionContext
+   ) { }
+
+   public getInstalledExtensions(): Array<Extension<unknown>> {
+      return extensions.all
+         .filter((extension) => {
+            return !extension.packageJSON.isBuiltin
+               && !isEqual(extension.id, this._extensionContext.extension.id);
+         });
    }
 
+   public getInstalledExtensionIds(): Array<string> {
+      return this.getInstalledExtensions()
+         .map((value) => {
+            return value.id;
+         });
+   }
 
+   public installExtensions(extensionIds: Array<string>) {
+
+   }
+
+   public uninstallExtensions(extensionIds: Array<string>) {
+
+   }
 }
