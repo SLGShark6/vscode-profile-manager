8e459d6c3d7970b9e7a17dd99775789836ea914b
Shane Gardner <gardnershane@outlook.com>
2022-05-23 05:15:36 +0000

    Renamde getChildProfiles to getProfileChildren

--- src/helpers/profile.helper.ts
+++ src/helpers/profile.helper.ts
@@ -264,7 +264,7 @@ export class ProfileHelper {
     * @returns - Found profile/ child storage object for the path provided
     * @throws Error if a profile doesn't exist at any of the nodes in the path
     */
-   public getChildProfiles(path?: string): Dictionary<string, Profile> {
+   public getProfileChildren(path?: string): Dictionary<string, Profile> {
       // Declare object to return
       let childrenStorage: Dictionary<string, Profile>;
       
